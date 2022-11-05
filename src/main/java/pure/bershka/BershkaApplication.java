56002e9a901c83c1988ab7993e244b5850594280
IEmreOzkayaI <0emre.ozkaya0@gmail.com>
2022-11-05 22:39:37 +0300

    preparing continue

---
+++ src/main/java/pure/bershka/BershkaApplication.java
@@ -0,0 +1,13 @@
+package pure.bershka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BershkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BershkaApplication.class, args);
+	}
+
+}
