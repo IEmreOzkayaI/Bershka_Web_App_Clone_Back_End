e5741ed5cb330f0926dfcb7089d9a8fe467f9a72
IEmreOzkayaI <0emre.ozkaya0@gmail.com>
2022-11-05 22:53:27 +0300

    All requirements prepared

--- src/main/java/pure/bershka/BershkaApplication.java
+++ src/main/java/pure/bershka/BershkaApplication.java
@@ -2,12 +2,19 @@ package pure.bershka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class BershkaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BershkaApplication.class, args);
 	}
 
+	@GetMapping("/hi")
+	public String den() {
+		return "Hello";
+	}
 }
