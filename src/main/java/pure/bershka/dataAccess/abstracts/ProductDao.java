e554148a7bce63215a9ca3d0fbf870760a759a36
IEmreOzkayaI <0emre.ozkaya0@gmail.com>
2022-11-09 23:50:17 +0300

    N-tier

--- src/main/java/pure/bershka/dataAccess/abstracts/ProductDao.java
+++ src/main/java/pure/bershka/dataAccess/abstracts/ProductDao.java
@@ -1,4 +1,4 @@
-package pure.bershka.dataAccess.concretes;
+package pure.bershka.dataAccess.abstracts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 import pure.bershka.entities.concretes.Product;
 
 @Repository
-public interface productService extends JpaRepository<Product, Integer> {
+public interface ProductDao extends JpaRepository<Product, Integer> {
 
 }
