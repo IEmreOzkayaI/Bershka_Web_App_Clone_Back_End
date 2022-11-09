9e0143513c9352ceba04b148c15d835e93a7ff22
IEmreOzkayaI <0emre.ozkaya0@gmail.com>
2022-11-10 00:04:07 +0300

    n-tier

--- src/main/java/pure/bershka/dataAccess/abstracts/ProductDao.java
+++ src/main/java/pure/bershka/dataAccess/abstracts/ProductDao.java
@@ -1,4 +1,4 @@
-package pure.bershka.dataAccess;
+package pure.bershka.dataAccess.abstracts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
