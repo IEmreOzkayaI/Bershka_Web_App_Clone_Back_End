54104e369a0ccef5faf2e1be1c3196df28ef1864
tunajuar <tunahan.kanpolat@ceng.deu.edu.tr>
2022-11-25 01:20:24 +0300

    services added

--- src/main/java/pure/bershka/business/concretes/ProductManager.java
+++ src/main/java/pure/bershka/business/concretes/ProductManager.java
@@ -1,5 +1,15 @@
 package pure.bershka.business.concretes;
 
-public class ProductManager {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pure.bershka.business.abstracts.ProductService;
+import pure.bershka.dataAccess.abstracts.ProductDao;
 
+@Service
+public class ProductManager implements ProductService {
+    private ProductDao productDao;
+    @Autowired
+    public ProductManager(ProductDao productDao) {
+        this.productDao = productDao;
+    }
 }
